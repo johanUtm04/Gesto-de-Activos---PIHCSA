16144a5819e1d485930e3f906b0e5bf867cfb19c
johanUtm04 <johanlopezrey1@gmail.com>
2026-01-03 15:38:08 -0600

    Monitor Funciona (CRUD) en el historial

--- app/Observers/MonitorObserver.php
+++ app/Observers/MonitorObserver.php
@@ -7,6 +7,7 @@ use App\Models\Monitor;
 use App\Models\Historial_log;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class MonitorObserver
 {
@@ -89,6 +90,40 @@ public function created(Monitor $monitor): void
     public function deleted(Monitor $monitor): void
     {
         //
+    }
+
+    public function deleting(Monitor $monitor): void
+    {
+        // 1.- Obtenemos el ID directamente de la columna, no de la relación es decir, $168 por ejemplo
+        $equipoId = $monitor->equipo_id; 
+
+        // 2. Buscamos el equipo de forma manual para asegurar que exista
+        //es decir buscamos ese registro en la tabla
+        $equipoPadre = \App\Models\Equipo::find($equipoId);
+
+        //3.- Si la Tomamos de Buena Manera crearemos un registro en Historial_Log
+        if ($equipoPadre) {
+            Historial_log::create([
+                'activo_id'         => $equipoPadre->id, // Vinculamos al ID del equipo
+                'usuario_accion_id' => \Illuminate\Support\Facades\Auth::id() ?? 1,
+                'tipo_registro'     => 'DELETE',
+                'detalles_json'     => [
+                    'mensaje'          => "COMPONENTE ELIMINADO: Se retiró un monitor del equipo",
+                    'usuario_asignado' => $equipoPadre->usuario->name ?? 'N/A',
+                    'rol'              => $equipoPadre->usuario->rol ?? 'N/A',
+                    'cambios'          => [
+                        'Monitor Retirado' => [
+                            'antes'   => "Marca: {$monitor->marca} | Serial: {$monitor->serial},
+                            | Escala en Pulgadas: {$monitor->escala_pulgadas} | Interface: {$monitor->interface}",
+                            'despues' => 'ELIMINADO'
+                        ]
+                    ],
+                    'respaldo' => $monitor->toArray() 
+                ]
+            ]);
+        } else {    //4.-En caso de Error
+            Log::warning("No se pudo crear log de eliminación: El monitor {$monitor->id} no tiene un equipo asociado.");
+        }
     }
 
     /**
